5fdeb217c1b89bcf7b597705572b8a3295a6ed3e
James.Hester <jxh@ansto.gov.au>
2019-12-09 15:02:26 +1100

    Explicit types for config types.

--- src/common.jl
+++ src/common.jl
@@ -1,15 +1,15 @@
 struct LexerConf
-    tokens
-    ignore
-    postlex
-    callbacks
+    tokens::Array{TerminalDef}
+    ignore::Array{String}
+    postlex  #Call the "process" method on this with the stream after lexing
+    callbacks::Dict{String,Function}
 end
 
 LexerConf(tokens;ignore=(),postlex=nothing,callbacks=Dict()) = LexerConf(tokens,ignore,postlex,callbacks)
 
 struct ParserConf
-    rules
-    callback
-    start
+    rules::Array{Rule}
+    callback::Dict{String,Function}
+    start::String #The name of the starting rule
 end
 
